8920f885d95b735b237283c0f56586ecd9de466e
trevor ngugi <ngugitrevor@gmail.com>
2022-11-26 01:26:07 +0300

    finished editing files

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app
 import pickle
 import numpy as np
 
-model=pickle.load(open('model.pkl','rb')) 
+
 modeltwo=pickle.load(open('modelfix.pkl','rb')) 
 @app.route('/')
 @app.route('/home')
@@ -14,9 +14,6 @@ def home():
 def index():
     return render_template('index.html', title='Home',data='hey')
 
-@app.route('/aisha')
-def trial():
-    return render_template('aisha.html', title='with gender',data='hey')
 
 @app.route("/predictiontwo",methods=["POST"])
 def predictiontwo():
@@ -25,15 +22,9 @@ def predictiontwo():
     gender=float(request.form['gender'])
     arr=np.array([[height,weight,gender]])
     pred=modeltwo.predict(arr)
-    return render_template('aishapred.html',data=pred)
-
-@app.route("/prediction",methods=["POST"])
-def prediction():
-    height=float(request.form['height'])
-    weight=float(request.form['weight'])
-    arr=np.array([[height,weight]])
-    pred=model.predict(arr)
     return render_template('prediction.html',data=pred)
+
+
 
 
 
